2ade2107ec9812d4d8acc58c9248b80e5342572a
Merijn Florie <34626@ma-web.nl>
2022-11-10 09:19:16 +0100

    eindopdracht

---
+++ Eindopdracht/Vragen eindproduct voorbeeld.py
@@ -0,0 +1,12 @@
+def Vragen():
+    vragen = input("Zijn er nog vragen? : ")
+    if vragen == "ja":
+        print("\nWat is je vraag?")
+    elif vragen =="nee":
+        print("\nEr zijn geen vragen")
+
+Vragen()
+
+
+
+
